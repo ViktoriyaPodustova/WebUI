3fbc68ba0a4acdb5cd0f35a4a034b208b080c8bd
ViktoriA <viktoriya1008@gmail.com>
2022-05-08 10:39:15 +0700

    Lesson3

---
+++ src/main/java/Lesson3/seleniumStart.java
@@ -0,0 +1,15 @@
+package Lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumStart {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.quit();
+    }
+}
